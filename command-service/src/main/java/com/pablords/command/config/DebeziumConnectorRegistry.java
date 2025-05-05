c82b43311fe7246a7386677e806e05374b466c7c
Pablo Ruan dos Santos <pablo.santos@telefonicati.onmicrosoft.com>
2025-05-05 13:32:57 -0300

    refactor: all

--- command-service/src/main/java/com/pablords/command/config/DebeziumConnectorRegistry.java
+++ command-service/src/main/java/com/pablords/command/config/DebeziumConnectorRegistry.java
@@ -4,7 +4,6 @@ import jakarta.annotation.PostConstruct;
 import lombok.extern.slf4j.Slf4j;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.*;
@@ -16,28 +15,19 @@ import java.util.Map;
 
 @Configuration
 @Slf4j
-@ConfigurationProperties(prefix = "debezium.connector")
 public class DebeziumConnectorRegistry {
 
   @Value("${debezium.connector.name}")
   private String connectorName;
-
   private final RestTemplate restTemplate;
-  private Map<String, String> config;
+  private final DebeziumConnectorProperties properties;
 
-  public DebeziumConnectorRegistry(RestTemplateBuilder builder) {
+  public DebeziumConnectorRegistry(RestTemplateBuilder builder, DebeziumConnectorProperties properties) {
     this.restTemplate = builder
         .connectTimeout(Duration.ofSeconds(5))
         .readTimeout(Duration.ofSeconds(10))
         .build();
-  }
-
-  public Map<String, String> getConfig() {
-    return config;
-  }
-
-  public void setConfig(Map<String, String> config) {
-    this.config = config;
+    this.properties = properties;
   }
 
   @PostConstruct
@@ -54,15 +44,11 @@ public class DebeziumConnectorRegistry {
       log.info("Conector '{}' ainda não está registrado. Registrando agora...", connectorName);
     }
 
-    log.info("Configuração enviada ao Kafka Connect: {}", config);
-
-    if (!config.containsKey("transforms.outbox.type")) {
-      log.error("A chave 'transforms.outbox.type' está ausente na configuração!");
-    }
+    log.info("Configuração enviada ao Kafka Connect: {}", properties.getConfig());
 
     Map<String, Object> requestBody = new HashMap<>();
     requestBody.put("name", connectorName);
-    requestBody.put("config", config);
+    requestBody.put("config", properties.getConfig());
 
     HttpHeaders headers = new HttpHeaders();
     headers.setContentType(MediaType.APPLICATION_JSON);
